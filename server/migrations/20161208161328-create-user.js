74c60130d819d6239e5f15aa957d5615956a90e3
Ethan Nwankwo <emekanwankwo201@gmail.com>
2016-12-09 15:52:01 +0100

    refactor(config.json): modify config.json file for travis to create db before build and running tests.

--- server/migrations/20161208161328-create-user.js
+++ server/migrations/20161208161328-create-user.js
@@ -1,44 +1,40 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        allowNull: false,
-        type: Sequelize.JSON
-      },
-      username: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      email: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      roleId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        defaultValue: 2
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.JSON
+    },
+    username: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    email: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    password: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    roleId: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      defaultValue: 2
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
 };
