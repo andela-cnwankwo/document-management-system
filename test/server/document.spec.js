9b9e48e2052021654ee3605b61a0701e00ec8db4
Ethan Nwankwo <emekanwankwo201@gmail.com>
2016-12-15 16:14:00 +0100

    fix(travis.yml): migrate command.

--- test/server/document.spec.js
+++ test/server/document.spec.js
@@ -9,59 +9,59 @@ let fakeUser;
 let currentUser;
 let fakeDocument;
 
-// describe('Document', () => {
-//   before((done) => {
-//     sequelize.sync({ }).then(() => {
-//       done();
-//     });
-//   });
+describe('Document', () => {
+  before((done) => {
+    sequelize.sync({ }).then(() => {
+      done();
+    });
+  });
 
-//   beforeEach((done) => {
-//     fakeDocument = factory.createDocument();
-//     fakeUser = factory.createUser();
-//     request(server).post('/users').send({ user: fakeUser }).expect(200)
-//       .then(() => {
-//         request(server).get('users/email').send({ user: fakeUser }).expect(200)
-//           .then((res) => {
-//             console.log('++++::::::+++', res.body, '()()()()', fakeUser);
-//             currentUser = res.body.user;
-//             done();
-//           });
-//       });
-//       console.log(currentUser);
-//   });
+  beforeEach((done) => {
+    fakeDocument = factory.createDocument();
+    fakeUser = factory.createUser();
+    request(server).post('/users').send({ user: fakeUser }).expect(200)
+      .then(() => {
+        request(server).get('users/email').send({ user: fakeUser }).expect(200)
+          .then((res) => {
+            console.log('++++::::::+++', res.body, '()()()()', fakeUser);
+            currentUser = res.body.user;
+            done();
+          });
+      });
+      console.log(currentUser);
+  });
 
-//   describe('Create Document', () => {
-//     it('should create a document with published date defined', (done) => {
-//       console.log('---:::::----', currentUser);
-//       fakeDocument.ownerId = currentUser.id;
-//       request(server).post('/documents').send({ document: fakeDocument })
-//       .expect(200)
-//         .then((res) => {
-//           expect(res.body.published).to.equal(fakeDocument.published);
-//           done();
-//         });
-//     });
+  describe('Create Document', () => {
+    it('should create a document with published date defined', (done) => {
+      console.log('---:::::----', currentUser);
+      fakeDocument.ownerId = currentUser.id;
+      request(server).post('/documents').send({ document: fakeDocument })
+      .expect(200)
+        .then((res) => {
+          expect(res.body.published).to.equal(fakeDocument.published);
+          done();
+        });
+    });
 
-//     it('should create a document with access as public by default ', (done) => {
-//       request(server).post('/documents').send({ document: fakeDocument })
-//       .expect(200)
-//         .then((res) => {
-//           expect(res.body.access).to.equal('public');
-//           done();
-//         });
-//     });
+    it('should create a document with access as public by default ', (done) => {
+      request(server).post('/documents').send({ document: fakeDocument })
+      .expect(200)
+        .then((res) => {
+          expect(res.body.access).to.equal('public');
+          done();
+        });
+    });
 
-//     it('should only retrieve private documents if requested by owner', (done) => {
-//       fakeDocument.ownerId = currentUser.id;
-//       request(server).post('/documents').send({ document: fakeDocument })
-//       .expect(200)
-//         .then(() => {
-//           request(server).get(`/documents?ownerId=${currentUser.id}`).expect(200)
-//             .then(() => {
-//               done();
-//             });
-//         });
-//     });
-//   });
-// });
+    it('should only retrieve private documents if requested by owner', (done) => {
+      fakeDocument.ownerId = currentUser.id;
+      request(server).post('/documents').send({ document: fakeDocument })
+      .expect(200)
+        .then(() => {
+          request(server).get(`/documents?ownerId=${currentUser.id}`).expect(200)
+            .then(() => {
+              done();
+            });
+        });
+    });
+  });
+});
