31bede296fc0684acee63c6c4381ada67a443436
Ethan Nwankwo <emekanwankwo201@gmail.com>
2016-12-15 18:38:39 +0100

    fix(migration-error): unable to migrate db roles in travis build

---
+++ settings/syncDB.js
@@ -0,0 +1,14 @@
+const env = process.env.NODE_ENV || 'test',
+  config = require('../settings/config.json')[env],
+  Sequelize = require('sequelize'),
+
+  // Initialize Sequelize with test configurations.
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host, dialect: config.dialect
+  });
+
+require('../app/models/user')(sequelize, Sequelize);
+require('../app/models/role')(sequelize, Sequelize);
+
+sequelize.sync({ force: true });
+
